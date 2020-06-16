9eaf305dfd85a332df0ce013fd27c82683d28f3b
salmane <salmane-ben@live.com>
2020-06-16 20:23:25 +0100

    trying

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
+        <link rel="stylesheet" href="css/mystyle.css">
 
         <!-- Styles -->
         <style>
@@ -80,21 +82,13 @@
             @endif
 
             <div class="content">
+              
                 <div class="title m-b-md">
-                    Laravel
+                    Will See
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+               
+            </div> 
         </div>
     </body>
 </html>
